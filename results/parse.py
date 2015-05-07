fb103781ee92a82e83791ff45b7b65498f1f5fec
Nate Brennand <natebrennand@gmail.com>
2015-05-07 01:31:20 -0400

    finish script

--- results/parse.py
+++ results/parse.py
@@ -1,5 +1,18 @@
 from sys import argv
+from itertools import groupby
 import re
+
+
+class Bench(object):
+    def __init__(self, title, num, length, clients, size, keep_alive, throughput, tiers):
+        self.title = title
+        self.num = num
+        self.length = length
+        self.clients = int(clients)
+        self.size = size
+        self.keep_alive = keep_alive
+        self.throughput = throughput
+        self.tiers = tiers
 
 
 def process_benchmark(f):
@@ -26,18 +39,41 @@ def process_benchmark(f):
 
     throughput = re.match(r"(?P<throughput>\d+\.\d+) requests per second", text).group("throughput")
     f.readline()
-    return (title, num, length, clients, size, keep_alive, throughput, tiers)
+    return Bench(title, num, length, clients, size, keep_alive, throughput, tiers)
 
 
-def gen_cdf(title, num, length, clients, size, keep_alive, throughput, tiers):
-    filename = "{title}_w_{clients}".format(title=title, clients=clients)
+def gen_cdf(b):
+    filename = "{title}_w_{clients}.cdf.results".format(title=b.title, clients=b.clients).replace("__", "_", 10)
     with open(filename, 'wb') as f:
-        for (percent, latency) in tiers:
-            f.write("({percent}, {latency})\n".format(percent=percent, latency=latency))
+        for (p, l) in b.tiers:
+            f.write("({percent}, {latency})\n".format(percent=p, latency=l))
 
 
 def gen_graph(res_array):
-    pass
+    commands = [r"^GET", r"^SET", r"^MSET", r"^PING_BULK_"]
+    pipelines = [r"pipeline_1_$", r"pipeline_10_$", r"pipeline_50_$"]
+
+    for filename in set(map(lambda a: a.title, res_array)):
+        with open(filename+".results", 'w') as f:
+            f.write("")
+
+
+    for c in commands:
+        results = filter(lambda r: re.match(c, r.title), res_array)
+
+        for p in pipelines:
+            results2 = filter(lambda r: re.search(p, r.title), results)
+
+            client_func = lambda r: r.clients
+            results2 = sorted(results2, key=client_func)
+            for c_num, nums in groupby(results2, key=client_func):
+                res = [r for r in nums]
+                results3 = max(res, key=lambda r: r.throughput)
+                print results3.title, c_num, results3.clients, results3.throughput, results3.num
+
+                with open(r.title+".results", 'a') as f:
+                    f.write("({}, {})\n".format(c_num, results3.throughput))
+
 
 
 if __name__ == '__main__':
@@ -55,4 +91,7 @@ if __name__ == '__main__':
             results.append(res)
 
     for r in results:
-        gen_cdf(*r)
+       gen_cdf(r)
+
+    gen_graph(results)
+
